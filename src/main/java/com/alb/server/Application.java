9b9453780e5c1dfaad0f207d4e2d77954541c1e0
jfelix <jfelix@localhost>
2018-06-26 14:32:52 -0300

    Adding support to https request.

--- src/main/java/com/alb/server/Application.java
+++ src/main/java/com/alb/server/Application.java
@@ -1,15 +1,40 @@
 package com.alb.server;
 
+import org.eclipse.jetty.http.HttpVersion;
 import org.eclipse.jetty.proxy.ConnectHandler;
+import org.eclipse.jetty.server.HttpConfiguration;
+import org.eclipse.jetty.server.HttpConnectionFactory;
+import org.eclipse.jetty.server.SecureRequestCustomizer;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.SslConnectionFactory;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
 
 public class Application {
-    
-    
+    private static int DEFAULT_HTTPS_SERVER_PORT = 8443;
+
+    private static final String PATH_TO_KEYSTORE = "certificate/alb.jks";
+    private static final String KEYSTORE_PASSWORD = "123456";
+
     public static void main(String... args) throws Exception {
-	Server server = new Server(8443);
+	Server server = new Server();
+
+	final HttpConfiguration httpConfiguration = new HttpConfiguration();
+	httpConfiguration.setSecureScheme("https");
+	httpConfiguration.setSecurePort(DEFAULT_HTTPS_SERVER_PORT);
+
+	final SslContextFactory sslContextFactory = new SslContextFactory(PATH_TO_KEYSTORE);
+	sslContextFactory.setKeyStorePassword(KEYSTORE_PASSWORD);
+
+	final HttpConfiguration httpsConfiguration = new HttpConfiguration(httpConfiguration);
+	httpsConfiguration.addCustomizer(new SecureRequestCustomizer());
+	final ServerConnector httpsConnector = new ServerConnector(server,
+		new SslConnectionFactory(sslContextFactory, HttpVersion.HTTP_1_1.asString()),
+		new HttpConnectionFactory(httpsConfiguration));
+	httpsConnector.setPort(DEFAULT_HTTPS_SERVER_PORT);
+	server.addConnector(httpsConnector);
 
 	ConnectHandler proxy = new ConnectHandler();
 	server.setHandler(proxy);
